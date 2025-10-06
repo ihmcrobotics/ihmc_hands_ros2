71f01eed0403a5daab6a425848f2734d7d913255
Alexander OCU <rosie@ihmc.org>
2025-10-06 14:08:06 -0500

    :arrow-up: ihmc-build 1.3.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
    plugins {
-      id("us.ihmc.ihmc-build") version "1.2.1"
+      id("us.ihmc.ihmc-build") version "1.3.0"
    }
 }
 
@@ -11,7 +11,7 @@ buildscript {
       mavenLocal()
    }
    dependencies {
-      classpath("us.ihmc:ihmc-build:1.2.1")
+      classpath("us.ihmc:ihmc-build:1.3.0")
       classpath("us.ihmc:ros2-msg-to-pubsub-generator:1.2.4")
    }
 }
