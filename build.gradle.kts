fef6c1d3aaea6b2f50e5aa1b537653ceca91ee1e
ihmc-rosie <rosie@ihmc.org>
2026-04-15 11:33:25 -0500

    :arrow_up: ihmc-robotics-tools 0.15.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ ihmc {
 
 mainDependencies {
    api("us.ihmc:ros2-library:1.2.5")
-   api("us.ihmc:ihmc-robotics-tools:0.15.7")
+   api("us.ihmc:ihmc-robotics-tools:0.15.8")
 }
 
 testDependencies {
